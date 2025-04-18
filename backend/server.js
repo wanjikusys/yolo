c2d3f49e6d2adb73f0b9d46f603274a4a12f1604
wanjikusys <leewama01@gmail.com>
2025-04-18 10:00:28 +0300

    Initial commit with working Dockerized backend and client

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://leena-ip-mongo:27017/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
